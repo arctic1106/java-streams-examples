317cc35ed58615be091df88e18d281596088f79a
Arctic <arcticsoft@outlook.com>
2021-09-21 00:57:08 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		var coche = new Car(1, "Ford","Mustang", LocalDate.of(1980, 5, 19));
+		var coche2 = new Car(2, "WV","Golf", LocalDate.of(1988, 9, 19));
+		var coche3 = new Car(3, "WV","Golf GTI", LocalDate.of(1987, 9, 19));
+		List<Car> carList = List.of(coche, coche2, coche3);
+		carList = carList.stream().filter(x -> x.marca().equals("WV")).toList();
+
+		Integer[] lista1 = {1,3,5,4,2};
+		List<Integer> l1 = Stream.of(lista1).map(x -> x * 2).sorted().filter(x -> x > 4).toList();
+
+		List<Integer> l2 = List.of(1,2,3,4,5);
+		l2 = l2.stream().map(x -> x * 2).toList();
+
+		System.out.println(carList);
+		System.out.println(l1);
+		System.out.println(l2);
+	}
+}
---
+++ Car.java
@@ -0,0 +1,4 @@
+import java.time.LocalDate;
+
+public record Car(int id, String marca, String modelo, LocalDate lanzamiento)
+{}
