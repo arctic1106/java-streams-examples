75c46e94a81f730355a3c01c448a78d95ce548fc
arctic1106 <arcticsoft@outlook.com>
2025-01-28 18:14:38 +0100

    review

--- Car.java
+++ Car.java
@@ -1,4 +1,4 @@
 import java.time.LocalDate;
 
-public record Car(int id, String marca, String modelo, LocalDate lanzamiento)
-{}
+public record Car(int id, String marca, String modelo, LocalDate lanzamiento) {
+}
